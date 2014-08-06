6b19f07f957b679b689280d801921d303d3569a9
Carlos Esparza <carlose@buybuy.com.mx>
2014-08-06 18:51:33 -0500

    Version Beta

---
+++ module/API/src/Shared/V1/REST/Functions/HttpResponse.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * HttpResponse
+ *
+ * @author Daniel Castanedo <daniel.b@hostdime.com.mx>
+ * @version 1.0
+ *
+ */
+
+namespace Shared\V1\REST\Functions;
+
+use Zend\Http\Response;
+
+class HttpResponse {
+
+    public function create($resource,array $halResource=null, array $halCollection=null){
+
+        $body = array(
+            "_links" => array(
+                "self" => array(
+                    "href" =>URL_API.'/v'.API_VERSION.'/'.RESOURCE.'/'.$resource->getPrimaryKey()
+                ),
+            ),
+        );
+
+    }
+}
