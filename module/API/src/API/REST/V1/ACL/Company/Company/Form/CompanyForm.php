0efde9710837417d484da8282c1f3628aea4f6e3
Daniel Castanedo <daniel.b@hostdime.com.mx>
2014-08-29 18:59:11 -0500

    version beta

--- module/API/src/API/REST/V1/ACL/Company/Company/Form/CompanyForm.php
+++ module/API/src/API/REST/V1/ACL/Company/Company/Form/CompanyForm.php
@@ -39,54 +39,6 @@ class CompanyForm extends Form
             ),
             
         ));
-        $this->add(array(
-            'type' => 'Hidden',
-            'name' => 'company_timezone',
-            'options' => array(
-                'label' => 'Zona horaria',
-            ),
-        ));
-        $this->add(array(
-            'type' => 'Select',
-            'name' => 'company_iso_codecountry',
-            'options' => array(
-                'label' => 'País',
-            ),
-        ));
-        $this->add(array(
-            'type' => 'Hidden',
-            'name' => 'company_address',
-            'options' => array(
-                'label' => 'Dirección',
-            ),
-        ));
-        $this->add(array(
-            'type' => 'Hidden',
-            'name' => 'company_address2',
-            'options' => array(
-                'label' => 'Colonia',
-            ),
-        ));
-        $this->add(array(
-            'type' => 'Hidden',
-            'name' => 'company_city',
-            'options' => array(
-                'label' => 'Ciudad',
-            ),
-        ));
-        $this->add(array(
-            'type' => 'Hidden',
-            'name' => 'company_state',
-            'options' => array(
-                'label' => 'Estado',
-            ),
-        ));
-        $this->add(array(
-            'type' => 'Hidden',
-            'name' => 'company_zipcode',
-            'options' => array(
-                'label' => 'Código postal',
-            ),
-        ));
+       
     }
 }
